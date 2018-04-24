88f78cf1753238c5a01e5119542b5d558e1ace47
leonid <leogansen@gmail.com>
2018-04-24 12:49:25 -0700

    Initial Commit

---
+++ MotionDetector/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MotionDetector
+//
+//  Created by Leonid on 4/24/18.
+//  Copyright © 2018 Leonid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
